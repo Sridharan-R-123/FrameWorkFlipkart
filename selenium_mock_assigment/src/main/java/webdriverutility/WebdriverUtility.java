a59762bc37646153c85279ba85273a438bacafe9
Sridharan R <Sridharan R@SRIDHARAN.mshome.net>
2025-09-25 12:52:36 +0530

    change

--- selenium_mock_assigment/src/main/java/webdriverutility/WebdriverUtility.java
+++ selenium_mock_assigment/src/main/java/webdriverutility/WebdriverUtility.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+/**
+ * @author Sridharan R
+ */
 public class WebdriverUtility {
 	
 	public void toMaximize(WebDriver driver) {
